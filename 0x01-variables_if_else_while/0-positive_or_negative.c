696bf20f2486fc0a964227e3c251662ec33b2d4a
Chiedozie195 <ugwudikeduke@gmail.com>
2022-04-12 12:11:35 -0700

    taskoo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,6 +7,7 @@
  *
  *Return: Always 0.
  */ 
+
 int main(void)
 {
 int n;
@@ -14,20 +15,22 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n > 1)
+if (n == 0)
 {	
-printf("%d is postive\n", n);
+printf("%i is zero\n", n);
 }
 
 else if (n < 0)
 {
-printf("%d is negative\n", n);
+printf("%i is negative\n", n);
 }
 
 else
 {
-printf("%d is zero\n", n);	
+printf("%i is positive\n", n);	
 }
 
 return (0);
+
+
 }
